2847a23b86a8b854a3b0870a1005bb9b1e8bd3af
Robert Grady <r2b2grady@gmail.com>
2016-06-19 13:03:53 -0400

    Week 3: Add Q6.

---
+++ week03/q6.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use v5.10.1;
+
+use strict;
+use warnings;
+
+while (1) {
+	print "Enter regex:  ";
+	my $re = <STDIN>;
+	print "\nEnter string, or 'exit' to exit:  ";
+	my $str = <STDIN>;
+	print "\n";
+	if ($str eq 'exit') {
+		print "So long, and thanks for all the fish!\n";
+		exit;
+	} else {
+		if ($str =~ m/$re/) {
+			print "Match!\n";
+		} else {
+			print "No match!\n";
+		}
+	}
+}
